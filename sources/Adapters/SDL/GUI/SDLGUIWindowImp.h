4b9590679054aa2940dd2ee9fb4a8bf195b5c8fa
Edoardo N <edne@gmx.com>
2023-11-24 21:33:42 +0100

    Fix tabs

--- sources/Adapters/SDL/GUI/SDLGUIWindowImp.h
+++ sources/Adapters/SDL/GUI/SDLGUIWindowImp.h
@@ -41,7 +41,7 @@ public: // Added functionality
 	void ProcessExpose() ;
 	void ProcessQuit() ;
 	void ProcessUserEvent(SDL_Event &event) ;
-    void LoadFont();
+	void LoadFont();
 protected:
 	void prepareFonts() ;
 	void prepareFullFonts() ;
@@ -71,6 +71,6 @@ private:
 	int appAnchorX_ ;
 	int appAnchorY_ ;
 	int mult_ ;
-    unsigned char font_[128*64];
+	unsigned char font_[128*64];
 } ;
 #endif
